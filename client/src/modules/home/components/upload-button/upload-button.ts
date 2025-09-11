65786470930dd4888d5742a1a980f5285cf11334
Andrii Lesiv <lesiv433@gmail.com>
2025-09-11 21:55:35 +0300

    upload button upd

--- client/src/modules/home/components/upload-button/upload-button.ts
+++ client/src/modules/home/components/upload-button/upload-button.ts
@@ -7,12 +7,24 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './upload-button.scss'
 })
 export class UploadButton {
+  selectedFile: File | null = null;
+
   @Output() fileUploaded = new EventEmitter<File>();
 
-  onFileSelected(event: any) {
-    const file: File = event.target.files[0];
-    if (file) {
-      this.fileUploaded.emit(file);
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
     }
+  }
+
+  onUpload() {
+    if (!this.selectedFile) {
+      alert('Please select a file first.');
+      return;
+    }
+
+    this.fileUploaded.emit(this.selectedFile);
+    this.selectedFile = null;
   }
 }
