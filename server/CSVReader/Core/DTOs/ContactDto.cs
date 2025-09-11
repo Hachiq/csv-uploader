08c729805578cff00406bb8e3afec6d1cc105ab1
Andrii Lesiv <lesiv433@gmail.com>
2025-09-11 16:19:47 +0300

    endpoints

---
+++ server/CSVReader/Core/DTOs/ContactDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.DTOs;
+
+public record ContactDto(
+    string Name,
+    DateTime DateOfBirth,
+    bool Married,
+    string Phone,
+    decimal Salary
+);
---
+++ server/CSVReader/Web/Controllers/ContactsController.cs
@@ -0,0 +1,35 @@
+﻿using Core.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadCsv(IFormFile file)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] ContactDto dto)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
