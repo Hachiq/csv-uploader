69c78ad534d8470b25f14f7e8459069e969760fc
Andrii Lesiv <lesiv433@gmail.com>
2025-09-11 20:32:30 +0300

    add sorting

--- client/src/modules/home/components/table/table.ts
+++ client/src/modules/home/components/table/table.ts
@@ -10,10 +10,46 @@ import { CommonModule } from '@angular/common';
 })
 export class Table {
   @Input() contacts: Contact[] = [];
+  sortedContacts: Contact[] = [];
+
+  sortColumn: keyof Contact | '' = '';
+  sortDirection: 'asc' | 'desc' = 'asc';
 
   @Output() onContactDelete = new EventEmitter<string>();
 
+  ngOnChanges() {
+    this.sortedContacts = [...this.contacts];
+  }
+
   deleteContact(id: string) {
     this.onContactDelete.emit(id);
+  }
+
+  sortBy(column: keyof Contact) {
+    console.log(`Sorting by ${column}`);
+    if (this.sortColumn === column) {
+      // toggle direction
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 'asc';
+    }
+
+    this.sortedContacts.sort((a, b) => {
+      const valueA = a[column];
+      const valueB = b[column];
+
+      if (valueA == null || valueB == null) return 0;
+
+      if (typeof valueA === 'string' && typeof valueB === 'string') {
+        return this.sortDirection === 'asc'
+          ? valueA.localeCompare(valueB)
+          : valueB.localeCompare(valueA);
+      }
+
+      return this.sortDirection === 'asc'
+        ? (valueA as any) > (valueB as any) ? 1 : -1
+        : (valueA as any) < (valueB as any) ? 1 : -1;
+    });
   }
 }
